4382bbd1dfa562cf921b3f14e0007a57a91cec50
Arnaud Lenglet <arlenglet@gmail.com>
2015-01-29 09:34:22 +0100

    Update conf before switch to new Mac

--- custom/alias.zsh
+++ custom/alias.zsh
@@ -9,12 +9,19 @@ alias reload="source ~/.zshrc"
 
 # Navigate to frequently used folders
 alias dev="cd ~/dev"
-alias api="cd ~/dev/church_office_manager_api/"
-alias api!="cd ~/dev/church_office_manager_api/ && vagrant up && vagrant ssh && cd /vagrant && rails s"
-alias front="cd ~/dev/church_office_manager_front/"
-alias front!="cd ~/dev/church_office_manager_front/ && grunt serve"
+alias api="cd ~/dev/yellow-umbrella-api/"
+alias api!="cd ~/dev/yellow-umbrella-api/ && guard"
+alias front="cd ~/dev/yellow-umbrella-front/"
+alias front!="cd ~/dev/yellow-umbrella-front/ && grunt serve"
+alias admin="cd ~/dev/yellow-umbrella-front-admin"
+alias admin!="cd ~/dev/yellow-umbrella-front-admin && grunt serve"
 
 # Git aliases
 alias prp="git pull --rebase && git push"
 alias cot="git pull"
 alias ppush="git push && git push gitlab master"
+
+# Git Rails aliases
+alias db:reset="powder down && rake db:drop --trace && rake db:create --trace && rake db:migrate --trace && powder up"
+alias db:resets="powder down && rake db:drop --trace && rake db:create --trace && rake db:migrate --trace && rake db:seed --trace && powder up"
+alias db:test:prepare="powder down && RAILS_ENV=test bundle exec rake db:drop --trace && RAILS_ENV=test bundle exec rake db:create --trace && RAILS_ENV=test bundle exec rake db:migrate --trace && powder up"
